a2473a7d4f60394f88f82292e72b8bdb9e4b7c9f
antonytitley-digital <254671107+antonytitley-digital@users.noreply.github.com>
2026-02-09 14:21:33 +0000

    Added PaymentAllocations files and paths

---
+++ test/connectors/PaymentAllocationsConnectorSpec.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import connectors.httpParsers.PaymentAllocationsHttpParser.{NotFoundResponse, UnexpectedResponse}
+import mocks.MockHttpV2
+import models.paymentAllocations.paymentAllocationsFull
+import play.api.http.Status.*
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.TestSupport
+
+class PaymentAllocationsConnectorSpec extends TestSupport with MockHttpV2 {
+
+  object TestPaymentAllocationsConnector extends PaymentAllocationsConnector(mockHttpClientV2, microserviceAppConfig)
+
+  val testNino: String = "AA000000B"
+  val testPaymentLot: String = "testPaymentLot"
+  val testPaymentLotItem: String = "testPaymentLotItem"
+  implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
+
+  "paymentAllocationsUrl" should {
+    "return the correct url" in {
+      val expectedUrl: String = s"${microserviceAppConfig.desUrl}/cross-regime/payment-allocation/NINO/$testNino/ITSA"
+      val actualUrl: String = TestPaymentAllocationsConnector.paymentAllocationsUrl(testNino)
+
+      actualUrl shouldBe expectedUrl
+    }
+  }
+
+  "queryParameters" should {
+    "return the correct formatted query parameters" in {
+      val expectedQueryParameters: Seq[(String, String)] = Seq(
+        "paymentLot" -> testPaymentLot,
+        "paymentLotItem" -> testPaymentLotItem
+      )
+      val actualQueryParameters: Seq[(String, String)] = TestPaymentAllocationsConnector.queryParameters(
+        paymentLot = testPaymentLot,
+        paymentLotItem = testPaymentLotItem
+      )
+
+      actualQueryParameters shouldBe expectedQueryParameters
+    }
+  }
+
+  "getPaymentAllocations" should {
+    "return payment allocations" when {
+      s"$OK is returned from the connector call with correct json" in {
+        setupMockHttpGetWithHeaderCarrier(
+          url = TestPaymentAllocationsConnector.paymentAllocationsUrl(testNino),
+          headers = microserviceAppConfig.desAuthHeaders,
+        )(
+          Right(paymentAllocationsFull)
+        )
+
+        val result = TestPaymentAllocationsConnector
+          .getPaymentAllocations(testNino, testPaymentLot, testPaymentLotItem).futureValue
+
+        result shouldBe Right(paymentAllocationsFull)
+      }
+    }
+
+    "return a not found response" when {
+      s"$NOT_FOUND is returned from the connector call" in {
+        setupMockHttpGetWithHeaderCarrier(
+          url = TestPaymentAllocationsConnector.paymentAllocationsUrl(testNino),
+          headers = microserviceAppConfig.desAuthHeaders
+        )(
+          Left(NotFoundResponse)
+        )
+
+        val result = TestPaymentAllocationsConnector.getPaymentAllocations(testNino, testPaymentLot, testPaymentLotItem).futureValue
+
+        result shouldBe Left(NotFoundResponse)
+      }
+    }
+
+    s"return an error" when {
+      "something went wrong" in {
+        setupMockHttpGetWithHeaderCarrier(
+          url = TestPaymentAllocationsConnector.paymentAllocationsUrl(testNino),
+          headers = microserviceAppConfig.desAuthHeaders
+        )(
+          Left(UnexpectedResponse)
+        )
+
+        val result = TestPaymentAllocationsConnector.getPaymentAllocations(testNino, testPaymentLot, testPaymentLotItem).futureValue
+
+        result shouldBe Left(UnexpectedResponse)
+      }
+    }
+  }
+}
---
+++ test/connectors/httpParsers/PaymentAllocationsHttpParserSpec.scala
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.httpParsers
+
+import connectors.httpParsers.PaymentAllocationsHttpParser.{NotFoundResponse, PaymentAllocationsReads, PaymentAllocationsResponse, UnexpectedResponse}
+import models.paymentAllocations.{AllocationDetail, PaymentAllocations}
+import play.api.http.Status.*
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.http.HttpResponse
+import utils.TestSupport
+
+import java.time.LocalDate
+
+class PaymentAllocationsHttpParserSpec extends TestSupport {
+
+  val paymentAllocations: PaymentAllocations = PaymentAllocations(
+    amount = Some(1000.00),
+    method = Some("method"),
+    reference = Some("reference"),
+    transactionDate = Some(LocalDate.parse("2022-06-23")),
+    allocations = Seq(
+      AllocationDetail(
+        transactionId = Some("transactionId"),
+        from = Some(LocalDate.parse("2022-06-23")),
+        to = Some(LocalDate.parse("2022-06-23")),
+        chargeType = Some("type"),
+        mainType = Some("mainType"),
+        amount = Some(1500.00),
+        clearedAmount = Some(500.00),
+        chargeReference = Some("chargeReference")
+      )
+    )
+  )
+
+  val paymentAllocationsJson: JsObject = Json.obj(
+    "paymentDetails" -> Json.arr(
+      Json.obj(
+        "paymentAmount" -> 1000.00,
+        "paymentMethod" -> "method",
+        "paymentReference" -> "reference",
+        "valueDate" -> LocalDate.parse("2022-06-23"),
+        "sapClearingDocsDetails" -> Json.arr(
+          Json.obj(
+            "sapDocNumber" -> "transactionId",
+            "taxPeriodStartDate" -> LocalDate.parse("2022-06-23"),
+            "taxPeriodEndDate" -> LocalDate.parse("2022-06-23"),
+            "chargeType" -> "type",
+            "mainType" -> "mainType",
+            "amount" -> 1500.00,
+            "clearedAmount" -> 500.00,
+            "chargeReference" -> "chargeReference"
+          )
+        )
+      )
+    )
+  )
+
+  val multiplePaymentAllocationJson: JsObject = Json.obj(
+    "paymentDetails" -> Json.arr(
+      Json.obj(
+        "paymentAmount" -> 1000.00,
+        "paymentMethod" -> "method",
+        "paymentReference" -> "reference",
+        "valueDate" -> LocalDate.parse("2022-06-23"),
+        "sapClearingDocsDetails" -> Json.arr(
+          Json.obj(
+            "sapDocNumber" -> "transactionId",
+            "taxPeriodStartDate" -> LocalDate.parse("2022-06-23"),
+            "taxPeriodEndDate" -> LocalDate.parse("2022-06-23"),
+            "chargeType" -> "type",
+            "mainType" -> "mainType",
+            "amount" -> 1500.00,
+            "clearedAmount" -> 500.00,
+            "chargeReference" -> "chargeReference"
+          )
+        )
+      ),
+      Json.obj(
+        "paymentAmount" -> 1000.00,
+        "paymentMethod" -> "method",
+        "paymentReference" -> "reference2",
+        "valueDate" -> LocalDate.parse("2022-06-23"),
+        "sapClearingDocsDetails" -> Json.arr(
+          Json.obj(
+            "sapDocNumber" -> "transactionId",
+            "taxPeriodStartDate" -> LocalDate.parse("2022-06-23"),
+            "taxPeriodEndDate" -> LocalDate.parse("2022-06-23"),
+            "chargeType" -> "type",
+            "mainType" -> "mainType",
+            "amount" -> 1500.00,
+            "clearedAmount" -> 500.00,
+            "chargeReference" -> "chargeReference"
+          )
+        )
+      )
+    )
+  )
+
+  "PaymentAllocationsHttpParser" should {
+    "return a payment allocations" when {
+      s"$OK is returned with valid json and a single payment allocations" in {
+        val httpResponse: HttpResponse = HttpResponse(OK, paymentAllocationsJson, Map.empty)
+
+        val expectedResult: PaymentAllocationsResponse = Right(paymentAllocations)
+        val actualResult: PaymentAllocationsResponse = PaymentAllocationsReads.read("", "", httpResponse)
+
+        actualResult shouldBe expectedResult
+      }
+      s"$OK is returned with valid json and multiple payment allocations" in {
+        val httpResponse: HttpResponse = HttpResponse(OK, multiplePaymentAllocationJson, Map.empty)
+
+        val expectedResponse: PaymentAllocationsResponse = Right(paymentAllocations)
+        val actualResult: PaymentAllocationsResponse = PaymentAllocationsReads.read("", "", httpResponse)
+
+        actualResult shouldBe expectedResponse
+      }
+    }
+    "return a Not Found response" when {
+      "the status of the response is 404" in {
+        val httpResponse: HttpResponse = HttpResponse(NOT_FOUND, "")
+
+        PaymentAllocationsReads.read("", "", httpResponse) shouldBe Left(NotFoundResponse)
+      }
+    }
+    s"return $UnexpectedResponse" when {
+      "no payment details are returned in the json" in {
+        val httpResponse: HttpResponse = HttpResponse(
+          OK,
+          Json.obj(
+            "paymentDetails" -> Json.arr()
+          ), Map.empty
+        )
+
+        val expectedResult: PaymentAllocationsResponse = Left(UnexpectedResponse)
+        val actualResult: PaymentAllocationsResponse = PaymentAllocationsReads.read("", "", httpResponse)
+
+        actualResult shouldBe expectedResult
+      }
+      "a 4xx status is returned" in {
+        val httpResponse: HttpResponse = HttpResponse(BAD_REQUEST, "")
+
+        val expectedResult: PaymentAllocationsResponse = Left(UnexpectedResponse)
+        val actualResult: PaymentAllocationsResponse = PaymentAllocationsReads.read("", "", httpResponse)
+
+        actualResult shouldBe expectedResult
+      }
+      "any other status is returned" in {
+        val httpResponse: HttpResponse = HttpResponse(INTERNAL_SERVER_ERROR, "")
+
+        val expectedResult: PaymentAllocationsResponse = Left(UnexpectedResponse)
+        val actualResult: PaymentAllocationsResponse = PaymentAllocationsReads.read("", "", httpResponse)
+
+        actualResult shouldBe expectedResult
+      }
+    }
+  }
+}
