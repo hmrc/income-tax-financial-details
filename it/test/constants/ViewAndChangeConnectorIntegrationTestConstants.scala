5346bcdea526f9335fb8eafa973a34baeec1e428
antonytitley-digital <254671107+antonytitley-digital@users.noreply.github.com>
2026-02-20 17:47:18 +0000

    Error with final test in ViewAndChangeConnectorISpec.scala - need to understand what the result should be

--- it/test/constants/ViewAndChangeConnectorIntegrationTestConstants.scala
+++ it/test/constants/ViewAndChangeConnectorIntegrationTestConstants.scala
@@ -25,6 +25,8 @@ import models.paymentAllocations.{AllocationDetail, PaymentAllocations}
 import play.api.http.Status.{CREATED, OK}
 import play.api.libs.json.{JsObject, Json}
 
+import java.time.LocalDate
+
 object ViewAndChangeConnectorIntegrationTestConstants {
 
   val getChargeHistoryResponseBody: JsObject =
