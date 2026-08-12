2c0fd80780c6358239580b1e1dc97f37b16511b8
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2026-08-12 09:15:47 +0100

    removes comments

--- app/models/financialDetails/hip/DocumentDetailHip.scala
+++ app/models/financialDetails/hip/DocumentDetailHip.scala
@@ -20,66 +20,34 @@ import play.api.libs.json.*
 
 import java.time.LocalDate
 
-// TODO: enable disabled field after migration to Scala 3 => MISUV-7996
 case class DocumentDetailHip(
-                              /* Format: YYYY */
                               taxYear: Int,
-                              /* SAP document number or Form Bundle Number for zero amount documents */
                               transactionId: String,
-                              /* If the document was created using the Form Bundle, the FB Number is provided */
                               formBundleNumber: Option[String] = None,
-                              /* Gives the reason as to why there is a credit on the account.  */
                               creditReason: Option[String] = None,
                               documentDate: LocalDate,
-                              /* Document Text */
                               documentText: Option[String] = None,
                               documentDueDate: Option[LocalDate] = None,
-                              /* Document descriptiom */
                               documentDescription: Option[String] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
-                              originalAmount: BigDecimal, // renamed from totalAmount
-                              /* Currency amount. 13-digits total with 2 decimal places */
-                              outstandingAmount: BigDecimal, // renamed from documentOutstandingAmount
-                              /* Currency amount. 13-digits total with 2 decimal places */
+                              originalAmount: BigDecimal,
+                              outstandingAmount: BigDecimal,
                               poaRelevantAmount: Option[BigDecimal] = None,
-//                              lastClearingDate: Option[LocalDate] = None,
-                              /* Last clearing reason */
-//                              lastClearingReason: Option[String] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               lastClearedAmount: Option[BigDecimal] = None,
-                              /* Y for Statistical. N for not */
                               statisticalFlag: String,
-                              /* Identifies a charge that has multiple items associated e.g. there are two BCD items due to coding occurring */
                               informationCode: Option[String] = None,
-                              //  /* Payment Lot */
                               paymentLot: Option[String] = None,
-                              /* Payment Lot Item */
                               paymentLotItem: Option[String] = None,
                               effectiveDateOfPayment: Option[LocalDate] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               accruingInterestAmount: Option[BigDecimal] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               interestRate: Option[BigDecimal] = None,
                               interestFromDate: Option[LocalDate] = None,
                               interestEndDate: Option[LocalDate] = None,
-                              /* Late Payment Interets Id */
                               latePaymentInterestId: Option[String] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               latePaymentInterestAmount: Option[BigDecimal] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               lpiWithDunningLock: Option[BigDecimal] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               interestOutstandingAmount: Option[BigDecimal] = None,
-                              /* Currency amount. 13-digits total with 2 decimal places */
                               amountCodedOut: Option[BigDecimal] = None,
                               chargeClassification: Option[String] = None
-                              /* If Charge has been reduced, and credit arises, document number to be shown */
-//                              documentNumberReducedCharge: Option[String] = None,
-                              /* Document name of charge reduced */
-//                              chargeTypeReducedCharge: Option[String] = None,
-//                              amendmentDateReducedCharge: Option[LocalDate] = None,
-                              /* Format: YYYY */
-//                              taxYearReducedCharge: Option[String] = None
                             )
 
 
